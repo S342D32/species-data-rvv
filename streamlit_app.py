6a316b81f5010fc20e2f095c82dd5b50a43f2ec1
Sourajit Nayakl <sourajitnayak3471@gmail.com>
2024-10-03 20:04:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,16 @@
 import streamlit as st
 import pandas as pd
 
-st.title("Wellcome Here")
+st.title("Welcome Here")
 st.write(
-    "Let's start building ML! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Let's start building ML! For help and inspiration, head over to docs.streamlit.io."
 )
 
 with st.expander('Data'):
     df = pd.read_csv("penguins.csv")
     st.write(df.head())
-    st.write(++x++)
-   
+    
+    # Initialize x with a valid value
     x = 0
     st.write(x + 1)  # Correct way to increment and display x
     
