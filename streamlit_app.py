82b8f2b34fdeaf84bde73e8a80d8b57803666a9c
Sourajit Nayakl <sourajitnayak3471@gmail.com>
2024-10-03 19:47:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,5 @@ st.title("Wellcome Here")
 st.write(
     "Let's start building ML! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+df=pd.read_csv("Iris.csv")
+df.head()
